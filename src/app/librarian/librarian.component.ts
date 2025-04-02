f32fe6d6a527cb9f7e47fa556322fcf8d129e47f
jyotisingh <jyotisinghhw@gmail.com>
2025-04-02 14:33:03 +0530

    fix the user

--- src/app/librarian/librarian.component.ts
+++ src/app/librarian/librarian.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class LibrarianComponent {
 
+  books: any[] = [];
+  newBook = { title: '', author: '', isbn: '', available: true };
+  selectedBook: any = null;
+
+  ngOnInit() {
+    this.books = JSON.parse(localStorage.getItem('books') || '[]');
+  }
+
+  addBook() {
+    this.books.push({ ...this.newBook });
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.newBook = { title: '', author: '', isbn: '', available: true };
+  }
+
+  editBook(book: any) {
+    this.selectedBook = { ...book };
+  }
+
+  updateBook() {
+    const index = this.books.findIndex(b => b.isbn === this.selectedBook.isbn);
+    if (index !== -1) {
+      this.books[index] = { ...this.selectedBook };
+      localStorage.setItem('books', JSON.stringify(this.books));
+      this.selectedBook = null;
+    }
+  }
+
+  deleteBook(isbn: string) {
+    this.books = this.books.filter(book => book.isbn !== isbn);
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
 }
+
+
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit{
+  books: any[] = [];
+  checkedOutBooks: any[] = [];
 
+  ngOnInit() {
+    this.books = JSON.parse(localStorage.getItem('books') || '[]');
+    this.checkedOutBooks = JSON.parse(localStorage.getItem('checkedOutBooks') || '[]');
+  }
+
+  checkoutBook(book: any) {
+    if (book.available) {
+      book.available = false;
+      this.checkedOutBooks.push(book);
+      localStorage.setItem('checkedOutBooks', JSON.stringify(this.checkedOutBooks));
+      localStorage.setItem('books', JSON.stringify(this.books));
+    }
+  }
+
+  returnBook(book: any) {
+    book.available = true;
+    this.checkedOutBooks = this.checkedOutBooks.filter(b => b.isbn !== book.isbn);
+    localStorage.setItem('checkedOutBooks', JSON.stringify(this.checkedOutBooks));
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
 }
+
